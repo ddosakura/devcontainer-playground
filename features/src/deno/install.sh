961f42b54c4119ef263dfcf73e98022380d8f17b
sakuramo <sakuramo@tencent.com>
2022-11-09 14:31:29 +0000

    WIP

--- features/src/deno/install.sh
+++ features/src/deno/install.sh
@@ -1,8 +1,12 @@
 export DENO_VERSION=${VERSION:-"latest"}
 export DVM_DIR=${DVMINSTALLPATH:-"/usr/local/share/dvm"}
 
+# TODO: fix  Permission denied
 curl -fsSL https://deno.land/x/dvm/install.sh | sh
-if [ "${DENO_VERSION}" != "" ]; then
+
+if [ "${NODE_VERSION}" = "latest" ]; then
+  "$DVM_DIR/bin/dvm" install
+elif [ "${DENO_VERSION}" != "none" ]; then
   "$DVM_DIR/bin/dvm" install $DENO_VERSION
 fi
 
--- features/test/deno/test.sh
+++ features/test/deno/test.sh
@@ -6,7 +6,10 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "tmp" ls /usr/local/share -la
+check "tmp1" ls /usr/local/share -la
+check "tmp2" ls $HOME -la
+check "tmp3" ls /root -la
+check "tmp4" ls /home -la
 check "dvm" /usr/local/share/dvm/bin/dvm ls
 check "deno" deno -V
 
