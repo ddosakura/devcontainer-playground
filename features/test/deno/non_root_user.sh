143d4f2498f6823b241ae7d8a9911435d1334afa
ddosakura <moyinzi@outlook.com>
2022-11-10 07:57:38 +0000

    fix(features/deno): user perm

--- features/test/deno/non_root_user.sh
+++ features/test/deno/non_root_user.sh
@@ -8,6 +8,7 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "version" deno -V
 check "dvm" bash -c "/usr/local/share/dvm/bin/dvm ls"
+check "perm" ls -lah /usr/local/share
 
 # Report result
 reportResults
